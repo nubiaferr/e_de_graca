9aac7c7fc08c5cfc50b75afa6bae2ed5b23e7ee1
nubiaferr <n.ferr@hotmail.com>
2021-07-29 12:05:39 -0300

    API

---
+++ BackEndEDG/src/main/java/com/EDG/BackEndEDG/BackEndEdgApplication.java
@@ -0,0 +1,13 @@
+package com.EDG.BackEndEDG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndEdgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndEdgApplication.class, args);
+	}
+
+}
---
+++ BackEndEDG/src/test/java/com/EDG/BackEndEDG/BackEndEdgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EDG.BackEndEDG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndEdgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
