dc883309fbfed635db492d5a35f15e2f46ad936a
nubiaferr <n.ferr@hotmail.com>
2021-08-12 13:09:55 -0300

    Deploy e documentação

--- BackEndEDG/src/main/java/com/EDG/BackEndEDG/BackEndEdgApplication.java
+++ BackEndEDG/src/main/java/com/EDG/BackEndEDG/BackEndEdgApplication.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
