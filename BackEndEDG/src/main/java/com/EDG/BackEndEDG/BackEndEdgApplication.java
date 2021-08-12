714e5f2ca5b538fb78f2e8b6514fc693c44863b0
nubiaferr <n.ferr@hotmail.com>
2021-08-12 10:49:01 -0300

    Documentação Swagger

--- BackEndEDG/src/main/java/com/EDG/BackEndEDG/BackEndEdgApplication.java
+++ BackEndEDG/src/main/java/com/EDG/BackEndEDG/BackEndEdgApplication.java
@@ -2,9 +2,23 @@ package com.EDG.BackEndEDG;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class BackEndEdgApplication {
+	@GetMapping
+	public ModelAndView swaggerUi() {
+	return new ModelAndView("redirect:/swagger-ui/");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndEdgApplication.class, args);
